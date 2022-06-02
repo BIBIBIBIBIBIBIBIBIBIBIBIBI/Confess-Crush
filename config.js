607df50d879bfe94e9de5a138667a2dee95ffb54
BIBIBIBIBIBIBIBIBIBIBIBIBI <106745100+BIBIBIBIBIBIBIBIBIBIBIBIBI@users.noreply.github.com>
2022-06-02 18:58:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Hi đây ko phải link hack thề luôn",
+    introTitle: 'Chào mấy pé',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Bạn có muốn ở chung phòng mình lúc đi chơi ko🥰',
+    desc: 'Bạn có muốn ở chung phòng mình lúc đi chơi ko🥰 ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Vì sao cậu lại muốn ở chung phòng với tớ :3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Ở thì ở mà không ở thì ở <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    messDesc: 'tối đó đi nhậu luôn ko :3',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://www.facebook.com/messages/t/100008324623158/' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
